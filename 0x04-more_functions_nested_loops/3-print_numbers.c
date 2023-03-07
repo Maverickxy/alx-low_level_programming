a907ab25f900d1a2e45b2f3514c7e40899429436
MaverickXY <aneleobinnaa@gmail.com>
2023-03-06 20:48:47 -0800

    0-memset.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,15 @@
-#include "main.h"
-
+nclude "main.h"
 /**
- * print_numbers - print  0 - 9
+ * print_numbers - prints the numbers
  *
- * Return: void
  */
-
 void print_numbers(void)
 {
-	char a = 0;
+	int i;
 
-	while (a <= 9)
+	for (i = '0'; i <= '9'; i++)
 	{
-		_putchar(a + '0');
-		a++;
+		_putchar(i);
 	}
 	_putchar('\n');
 }
